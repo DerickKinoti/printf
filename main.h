4d7f678a036cd7914f0a918dcfdffa5aa706028d
MaryMonari <moraamarymonari@gmail.com>
2022-08-11 22:48:04 -0700

    main.h updates

--- main.h
+++ main.h
@@ -77,8 +77,12 @@ int print_non_printable(va_list types, char buffer[],
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Function to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
