0da3de58cb6112da72902b2c009d617f2ed9e55b
MaryMonari <moraamarymonari@gmail.com>
2022-08-11 22:48:43 -0700

    main.h updates

--- main.h
+++ main.h
@@ -86,3 +86,7 @@ int get_size(const char *format, int *i);
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
